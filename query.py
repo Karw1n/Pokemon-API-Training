f466ea30b6166b62438688b36b408fb26bf22112
Karw1n <akbbalfour@gmail.com>
2026-02-24 14:15:48 +0000

    Edited the get command to retrieve 'pokemon-species'

--- query.py
+++ query.py
@@ -2,14 +2,15 @@ import requests
 
 name = input("Enter Pokemon Name: ")
 
-url = f"https://pokeapi.co/api/v2/{name.lower()}/"
+url = f"https://pokeapi.co/api/v2/pokemon-species/{name.lower()}/"
 response = requests.get(url)
 
 if response.status_code == 200:
     data = response.json()
     print(f"Name: {data['name']}")
-    print(f"Height: {data['height']}")
-    print(f"Weight: {data['weight']}")
+    print(f"Id: {data['id']}")
+    print(f"Generation: {data['generation']['name']}")
+    # print(f"Pokemon Entries: {data['pokemon_entries']}")
 else:
     print("Pokemon not found.")
     
