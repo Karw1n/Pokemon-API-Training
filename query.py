bc7402378ee657f5b053fd06b4073eb79354168c
Karw1n <akbbalfour@gmail.com>
2026-02-24 14:39:25 +0000

    Changed endpoint to 'pokemon' to retrieve different information.

--- query.py
+++ query.py
@@ -2,15 +2,21 @@ import requests
 
 name = input("Enter Pokemon Name: ")
 
-url = f"https://pokeapi.co/api/v2/pokemon-species/{name.lower()}/"
+url = f"https://pokeapi.co/api/v2/pokemon/{name.lower()}/"
 response = requests.get(url)
 
 if response.status_code == 200:
     data = response.json()
+    # print(data)
     print(f"Name: {data['name']}")
-    print(f"Id: {data['id']}")
-    print(f"Generation: {data['generation']['name']}")
-    # print(f"Pokemon Entries: {data['pokemon_entries']}")
-else:
+    print(f"Id: {data['id']}")   
+    print(f"Types: {[type['type']['name'] for type in data['types']]}") 
+    print(f"Sprite URL: {data['sprites']['front_default']}")
+elif response.status_code == 404:
     print("Pokemon not found.")
-    
+elif response.status_code == 500:
+    print("Internal server error.")
+else:
+    print(f"An error occurred. Status code: {response.status_code}")
+
+
