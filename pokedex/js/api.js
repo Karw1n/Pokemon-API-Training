75eaa8aefd48ea0a6658d30d096f924f64c6216a
Karw1n <akbbalfour@gmail.com>
2026-03-09 13:38:20 +0000

    Add parameter handling and implement try-catch error handling.

--- pokedex/js/api.js
+++ pokedex/js/api.js
@@ -1,7 +1,15 @@
-async function getPokemon() {
-    const name = "snivy";
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}/`);
-    const data = await response.json();
-
-    console.log(data);
+async function fetchPokemon(pokemonName) {
+    try {
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName.toLowerCase()}/`);
+        
+        // Request valid but input is not
+        if (!response.ok) {
+            throw new Error("Pokemon not found");
+        }
+        const data = await response.json();
+        return data;
+    
+    } catch (error) {
+        console.error(error);
+    }
 }
