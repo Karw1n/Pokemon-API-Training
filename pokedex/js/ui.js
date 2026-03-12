21974344166c478a2c9e9174db48744eb3cc62ed
Karw1n <akbbalfour@gmail.com>
2026-03-12 10:43:05 +0000

    Added coloured background to Types text

--- pokedex/js/ui.js
+++ pokedex/js/ui.js
@@ -15,13 +15,44 @@ export function renderPokemon(pokemon) {
     const height = document.createElement("p");
     height.textContent = `Height: ${p_height}`;
 
-    const types = document.createElement("p")
-    types.textContent = `Types: ${p_types.join(", ")}`;
+    const types_container = document.createElement("div");
+    const types_label = document.createElement("p");
+    types_label.textContent = "Types:";
 
+    p_types.forEach(type => {
+        const badge = document.createElement("span");
+        badge.textContent = type;
+
+        badge.style.backgroundColor = typeColors[type];
+
+        types_container.appendChild(badge);
+    })
+    
     card.append(sprite);
     card.appendChild(name);
     card.appendChild(height);
-    card.appendChild(types);
+    card.appendChild(types_label);
+    card.appendChild(types_container);
 
     return card
 }
+
+const typeColors = {
+    fire: "#F08030",
+    water: "#6890F0",
+    grass: "#78C850",
+    electric: "#F8D030",
+    ice: "#98D8D8",
+    fighting: "#C03028",
+    poison: "#A040A0",
+    ground: "#E0C068",
+    flying: "#A890F0",
+    psychic: "#F85888",
+    bug: "#A8B820",
+    rock: "#B8A038",
+    ghost: "#705898",
+    dragon: "#7038F8",
+    dark: "#705848",
+    steel: "#B8B8D0",
+    fairy: "#EE99AC"
+};
