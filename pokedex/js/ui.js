d5221d7336f9f5eac9c4ba931bfcf21cf2940160
Karw1n <akbbalfour@gmail.com>
2026-03-12 10:01:56 +0000

    Added image display of Pokemon Spritie

--- pokedex/js/ui.js
+++ pokedex/js/ui.js
@@ -1,9 +1,13 @@
 export function renderPokemon(pokemon) {
+    const p_sprite = pokemon.sprites.front_default;
     const p_name = pokemon.name;
     const p_height = pokemon.height;
     const p_types = pokemon.types.map(t => t.type.name);
 
     const card = document.createElement("div");
+
+    const sprite = document.createElement("img");
+    sprite.src = pokemon.sprites.front_default;
 
     const name = document.createElement("p");
     name.textContent = `Name: ${p_name}`;
@@ -14,6 +18,7 @@ export function renderPokemon(pokemon) {
     const types = document.createElement("p")
     types.textContent = `Types: ${p_types.join(", ")}`;
 
+    card.append(sprite);
     card.appendChild(name);
     card.appendChild(height);
     card.appendChild(types);
