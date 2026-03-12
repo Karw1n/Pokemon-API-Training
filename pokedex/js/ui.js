4c190381724f97778c22765b953060646524372c
Karw1n <akbbalfour@gmail.com>
2026-03-12 10:55:00 +0000

    Updated ui.js from snake case to Camel Case

--- pokedex/js/ui.js
+++ pokedex/js/ui.js
@@ -1,38 +1,38 @@
 export function renderPokemon(pokemon) {
-    const p_sprite = pokemon.sprites.front_default;
-    const p_name = pokemon.name;
-    const p_height = pokemon.height;
-    const p_types = pokemon.types.map(t => t.type.name);
+    const pSprite = pokemon.sprites.front_default;
+    const pName = pokemon.name;
+    const pHeight = pokemon.height;
+    const pTypes = pokemon.types.map(t => t.type.name);
 
     const card = document.createElement("div");
 
     const sprite = document.createElement("img");
-    sprite.src = pokemon.sprites.front_default;
+    sprite.src = pSprite;
 
     const name = document.createElement("p");
-    name.textContent = `Name: ${p_name}`;
+    name.textContent = `Name: ${pName}`;
 
     const height = document.createElement("p");
-    height.textContent = `Height: ${p_height}`;
+    height.textContent = `Height: ${pHeight}`;
 
-    const types_container = document.createElement("div");
-    const types_label = document.createElement("p");
-    types_label.textContent = "Types:";
+    const typesContainer = document.createElement("div");
+    const typesLabel = document.createElement("p");
+    typesLabel.textContent = "Types:";
 
-    p_types.forEach(type => {
+    pTypes.forEach(type => {
         const badge = document.createElement("span");
         badge.textContent = type;
 
         badge.style.backgroundColor = typeColors[type];
 
-        types_container.appendChild(badge);
+        typesContainer.appendChild(badge);
     })
     
     card.append(sprite);
     card.appendChild(name);
     card.appendChild(height);
-    card.appendChild(types_label);
-    card.appendChild(types_container);
+    card.appendChild(typesLabel);
+    card.appendChild(typesContainer);
 
     return card
 }
